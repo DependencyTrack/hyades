594caae75cff0721300abe1f7ac42eebd99fccb4
mehab <meha.bhargava2@gmail.com>
2022-10-06 10:56:09 +0100

    Feature/pr fixes (#14)
    * removed not required topic
    
    * added flatmap instead of flatmapvalues
    
    * trying with return null
    
    * another refactor
    
    * working fine and refactoring done

--- src/main/java/org/acme/consumer/PrimaryConsumer.java
+++ src/main/java/org/acme/consumer/PrimaryConsumer.java
@@ -9,23 +9,27 @@ import io.quarkus.kafka.client.serialization.ObjectMapperSerde;
 import io.quarkus.runtime.StartupEvent;
 import org.acme.event.VulnerabilityAnalysisEvent;
 import org.acme.model.Component;
-import org.acme.serde.VulnerabilityAnalysisEventDeserializer;
-import org.acme.serde.VulnerabilityAnalysisEventSerializer;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.KStream;
+import org.checkerframework.checker.units.qual.A;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 @ApplicationScoped
 public class PrimaryConsumer {
-    
+
     @ConfigProperty(name = "consumer.primary.applicationId")
     String applicationId;
 
@@ -55,14 +59,21 @@ public class PrimaryConsumer {
                 VulnerabilityAnalysisEvent.class);
         ObjectMapperSerde<Component> componentSerde = new ObjectMapperSerde<>(Component.class);
 
-        KStream<String, VulnerabilityAnalysisEvent> kStreamsVulnTask = builder.stream(inputTopic, Consumed.with(Serdes.String(),vulnerabilityAnalysisEventSerde )); //receiving the message on topic event
-
-        //Splitting the incoming message into number of components
+        KStream<String, VulnerabilityAnalysisEvent> kStreamsVulnTask = builder.stream(inputTopic, Consumed.with(Serdes.String(), vulnerabilityAnalysisEventSerde)); //receiving the message on topic event
         //Setting the message key same as the name of component to send messages on different partitions of topic event-out
-        KStream<String, Component> splittedStreams = kStreamsVulnTask.flatMapValues(value -> value.getComponents()).selectKey((key, value) -> value.getName());
-
+        KStream<String, Component> splittedStreams = kStreamsVulnTask.flatMap((s, vulnerabilityAnalysisEvent) -> {
+            List<Component> components = vulnerabilityAnalysisEvent.getComponents();
+            if (components.isEmpty()) {
+                return Collections.emptyList();
+            } else {
+                ArrayList<KeyValue<String, Component>> componentList = new ArrayList<>();
+                for (Component component : components) {
+                    componentList.add(KeyValue.pair(component.getName(), component));
+                }
+                return componentList;
+            }
+        });
         splittedStreams.to(eventTopic, (Produced<String, Component>) Produced.with(Serdes.String(), componentSerde));
-
         streams = new KafkaStreams(builder.build(), props);
         streams.start();
 
