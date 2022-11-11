77acd19d75e282a9f3719046bb8f53580072a448
nscuro <nscuro@protonmail.com>
2022-11-11 13:00:45 +0100

    Integrate notification consumer into existing topology
    Having multiple separate streams topologies isn't really supported with Quarkus
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/acme/common/ApplicationProperty.java
+++ src/main/java/org/acme/common/ApplicationProperty.java
@@ -1,10 +1,7 @@
 package org.acme.common;
 
 import io.smallrye.config.ConfigMapping;
-import org.apache.kafka.common.protocol.types.Field;
 
 @ConfigMapping(prefix = "poc")
 public interface ApplicationProperty {
-    String bootstrapServer();
-    String notificationApplicationId();
 }
