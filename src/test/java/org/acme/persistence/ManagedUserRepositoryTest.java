d3e22f718a376d3df5fce64a41b4e0ab2c22e966
nscuro <nscuro@protonmail.com>
2022-11-11 18:48:45 +0100

    Fix repository tests
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/acme/persistence/ManagedUserRepositoryTest.java
+++ src/test/java/org/acme/persistence/ManagedUserRepositoryTest.java
@@ -18,26 +18,47 @@ class ManagedUserRepositoryTest {
     @Inject
     ManagedUserRepository repository;
 
+
     @Test
     @TestTransaction
+    @SuppressWarnings("unchecked")
     void testFindEmailsByTeam() {
-        entityManager.createNativeQuery("""
+        final List<Integer> teamIds = (List<Integer>) entityManager.createNativeQuery("""
                 INSERT INTO "TEAM" ("NAME", "UUID") VALUES 
                     ('foo', 'ba38e779-e252-4033-8e76-156dc46cc7a6'),
-                    ('bar', '507d8f3c-431d-47aa-929e-7647746d07a9');
+                    ('bar', '507d8f3c-431d-47aa-929e-7647746d07a9')
+                RETURNING "ID";
+                """).getResultList();
+        final int teamFooId = teamIds.get(0);
+        final int teamBarId = teamIds.get(1);
+
+        final List<Integer> userIds = (List<Integer>) entityManager.createNativeQuery("""
                 INSERT INTO "MANAGEDUSER" ("EMAIL", "PASSWORD", "FORCE_PASSWORD_CHANGE", "LAST_PASSWORD_CHANGE", "NON_EXPIRY_PASSWORD", "SUSPENDED") VALUES 
                     ('foo@example.com', 'foo', false, NOW(), true, false),
                     ('bar@example.com', 'bar', false, NOW(), true, false),
                     ('baz@example.com', 'baz', false, NOW(), true, false),
-                    (NULL, 'qux', false, NOW(), true, false);
-                INSERT INTO "MANAGEDUSERS_TEAMS" ("MANAGEDUSER_ID", "TEAM_ID") VALUES
-                    (1, 1),
-                    (2, 1),
-                    (4, 1),
-                    (2, 2);
-                """).executeUpdate();
+                    (NULL, 'qux', false, NOW(), true, false)
+                RETURNING "ID";
+                """).getResultList();
+        final int userFooId = userIds.get(0);
+        final int userBarId = userIds.get(1);
+        final int userQuxId = userIds.get(3);
 
-        final List<String> users = repository.findEmailsByTeam(1);
+        entityManager.createNativeQuery("""
+                        INSERT INTO "MANAGEDUSERS_TEAMS" ("MANAGEDUSER_ID", "TEAM_ID") VALUES
+                            (:userFooId, :teamFooId),
+                            (:userBarId, :teamFooId),
+                            (:userQuxId, :teamFooId),
+                            (:userBarId, :teamBarId);
+                        """)
+                .setParameter("userFooId", userFooId)
+                .setParameter("userBarId", userBarId)
+                .setParameter("userQuxId", userQuxId)
+                .setParameter("teamFooId", teamFooId)
+                .setParameter("teamBarId", teamBarId)
+                .executeUpdate();
+
+        final List<String> users = repository.findEmailsByTeam(teamFooId);
         Assertions.assertEquals(2, users.size());
         Assertions.assertEquals("foo@example.com", users.get(0));
         Assertions.assertEquals("bar@example.com", users.get(1));
--- src/test/java/org/acme/persistence/TeamRepositoryTest.java
+++ src/test/java/org/acme/persistence/TeamRepositoryTest.java
@@ -21,21 +21,37 @@ class TeamRepositoryTest {
 
     @Test
     @TestTransaction
+    @SuppressWarnings("unchecked")
     void testFindByNotificationRule() {
-        entityManager.createNativeQuery("""
+        final List<Integer> teamIds = (List<Integer>) entityManager.createNativeQuery("""
                 INSERT INTO "TEAM" ("NAME", "UUID") VALUES 
                     ('foo', 'fa26b29f-e106-4d62-b1a3-2073b63c9dd0'),
                     ('bar', 'c18d0094-f161-4581-96fa-bfc7e413c78d'),
-                    ('baz', '6db9c0cb-9c84-440a-89a8-9bbed5d028d9');
+                    ('baz', '6db9c0cb-9c84-440a-89a8-9bbed5d028d9')
+                RETURNING "ID";
+                """).getResultList();
+        final int teamFooId = teamIds.get(0);
+        final int teamBarId = teamIds.get(1);
+
+        final List<Integer> ruleIds = (List<Integer>) entityManager.createNativeQuery("""
                 INSERT INTO "NOTIFICATIONRULE" ("ENABLED", "NAME", "NOTIFY_CHILDREN", "SCOPE", "UUID") VALUES
                     (true, 'foo', false, 'PORTFOLIO', '6b1fee41-4178-4a23-9d1b-e9df79de8e62'),
-                    (true, 'bar', false, 'PORTFOLIO', 'ee74dc70-cd8e-41df-ae6a-1093d5f7b608');
-                INSERT INTO "NOTIFICATIONRULE_TEAMS" ("NOTIFICATIONRULE_ID", "TEAM_ID") VALUES
-                    (1, 1), 
-                    (1, 2);
-                """).executeUpdate();
+                    (true, 'bar', false, 'PORTFOLIO', 'ee74dc70-cd8e-41df-ae6a-1093d5f7b608')
+                RETURNING "ID";
+                """).getResultList();
+        final int ruleFooId = ruleIds.get(0);
 
-        final List<Team> teams = repository.findByNotificationRule(1);
+        entityManager.createNativeQuery("""                            
+                        INSERT INTO "NOTIFICATIONRULE_TEAMS" ("NOTIFICATIONRULE_ID", "TEAM_ID") VALUES
+                            (:ruleFooId, :teamFooId), 
+                            (:ruleFooId, :teamBarId);
+                        """)
+                .setParameter("ruleFooId", ruleFooId)
+                .setParameter("teamFooId", teamFooId)
+                .setParameter("teamBarId", teamBarId)
+                .executeUpdate();
+
+        final List<Team> teams = repository.findByNotificationRule(ruleFooId);
         Assertions.assertEquals(2, teams.size());
         Assertions.assertEquals("foo", teams.get(0).getName());
         Assertions.assertEquals("bar", teams.get(1).getName());
