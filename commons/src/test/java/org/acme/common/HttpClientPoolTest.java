e20b8efb0aea0738644e4c30e9d26d7f8dab8f64
Apurva <apurva.jain@gatech.edu>
2022-11-25 13:13:23 -0600

    test cases added for util/common/notification classes in commons module
    Signed-off-by: Apurva <apurva.jain@gatech.edu>

---
+++ commons/src/test/java/org/acme/common/HttpClientPoolTest.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.acme.common;
+
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class HttpClientPoolTest {
+
+    @Test
+    public void getClientTest() {
+        CloseableHttpClient client = HttpClientPool.getClient();
+        Assertions.assertNotNull(client);
+    }
+
+}
