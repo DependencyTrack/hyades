7e7dd260454e63e943ba0ecccdd20f54a7d06e22
Niklas <nscuro@protonmail.com>
2022-11-30 17:58:25 +0100

    Port repository analyzer tests from API server (#140)
    * Port repository analyzer tests from API server
    
    Also fix bug in URL building
    
    Signed-off-by: nscuro <nscuro@protonmail.com>
    
    * Make test class visibility package-private
    
    In order to reduce Sonar code smells
    
    Signed-off-by: nscuro <nscuro@protonmail.com>
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- repository-meta-analyzer/src/main/java/org/acme/repositories/NpmMetaAnalyzer.java
+++ repository-meta-analyzer/src/main/java/org/acme/repositories/NpmMetaAnalyzer.java
@@ -73,7 +73,7 @@ public class NpmMetaAnalyzer extends AbstractMetaAnalyzer {
                 packageName = component.getPurl().getName();
             }
 
-            final String url = String.format(baseUrl, API_URL, packageName);
+            final String url = String.format(baseUrl + API_URL, packageName);
             try {
                 final HttpRequest<GetRequest> request = ui.get(url)
                         .header("accept", "application/json");
