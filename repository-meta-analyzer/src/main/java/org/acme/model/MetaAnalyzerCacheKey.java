0d2fe71436d229eb4514e0fe679350f6e6636d4d
Sahiba Mittal <sahibamittal98@gmail.com>
2022-12-14 15:11:43 +0530

    Update MetaAnalyzerCacheKey.java

--- repository-meta-analyzer/src/main/java/org/acme/model/MetaAnalyzerCacheKey.java
+++ repository-meta-analyzer/src/main/java/org/acme/model/MetaAnalyzerCacheKey.java
@@ -5,11 +5,19 @@ import org.acme.repositories.IMetaAnalyzer;
 
 public class MetaAnalyzerCacheKey {
 
-    private final IMetaAnalyzer analyzer;
-    private final PackageURL purl;
+    private IMetaAnalyzer analyzer;
+    private PackageURL purl;
 
     public MetaAnalyzerCacheKey(final IMetaAnalyzer analyzer, final PackageURL purl) {
         this.analyzer = analyzer;
         this.purl = purl;
+    }
+
+    public IMetaAnalyzer getAnalyzer() {
+        return analyzer;
+    }
+
+    public PackageURL getPurl() {
+        return purl;
     }
 }
