1d89ecae8f24b88e1e08b3aca0916a84c815a75a
mehab <meha.bhargava2@gmail.com>
2022-12-16 17:54:37 +0000

    added code changes for creation of graalvm native binary. Yet to resolve this error system properties: No configuration setting found for key 'caffeine'. Will look into quarkus based caching option also

--- commons/src/main/java/org/acme/cache/CacheConfig.java
+++ commons/src/main/java/org/acme/cache/CacheConfig.java
@@ -1,6 +1,7 @@
 package org.acme.cache;
 
 import com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 import javax.cache.CacheManager;
 import javax.cache.Caching;
@@ -8,8 +9,9 @@ import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 
 @Dependent
+@RegisterForReflection(classNames = { "com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider" })
 class CacheConfig {
-    
+
     @Produces
     CacheManager cacheManager() {
         return Caching
