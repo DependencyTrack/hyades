5cf7bcaa49587144f229de9cfeaf9b707dc4881a
mehab <meha.bhargava2@gmail.com>
2022-12-19 20:36:52 +0000

    intermediate commit. Cache changes implemented and native binary created successdfully. Unit test needs to be updated for verifying cache results

--- commons/src/main/java/org/acme/cache/CacheConfig.java
+++ commons/src/main/java/org/acme/cache/CacheConfig.java
@@ -1,22 +1,22 @@
 package org.acme.cache;
-
-import com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider;
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-import javax.cache.CacheManager;
-import javax.cache.Caching;
-import javax.enterprise.context.Dependent;
-import javax.enterprise.inject.Produces;
-
-@Dependent
-@RegisterForReflection(classNames = { "com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider" })
-class CacheConfig {
-
-    @Produces
-    CacheManager cacheManager() {
-        return Caching
-                .getCachingProvider(CaffeineCachingProvider.class.getName())
-                .getCacheManager();
-    }
-
-}
+//
+//import com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider;
+//
+//import javax.cache.CacheManager;
+//import javax.cache.Caching;
+//import javax.enterprise.context.Dependent;
+//import javax.enterprise.inject.Produces;
+//import io.quarkus.
+//import io.quarkus.cache.CacheManager;
+//
+//@Dependent
+//class CacheConfig {
+//
+//    @Produces
+//    CacheManager cacheManager() {
+//        return Caching
+//                .getCachingProvider(CaffeineCachingProvider.class.getName())
+//                .getCacheManager();
+//    }
+//
+//}
