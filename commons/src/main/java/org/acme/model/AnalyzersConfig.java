f397c8db17903e06f41491a731fee218d5f17fc1
Apurva <apurva.jain@gatech.edu>
2022-12-23 08:39:24 -0600

    Wip : Analyzer completion
    Signed-off-by: Apurva <apurva.jain@gatech.edu>

---
+++ commons/src/main/java/org/acme/model/AnalyzersConfig.java
@@ -0,0 +1,43 @@
+package org.acme.model;
+
+import java.io.Serializable;
+
+public class AnalyzersConfig implements Serializable {
+    private boolean snykEnabled;
+    private boolean OSSEnabled;
+    private boolean internalAnalyzerEnabled;
+
+    public AnalyzersConfig(boolean snykEnabled, boolean OSSEnvuabled, boolean internalAnalyzerEnabled) {
+        this.snykEnabled = snykEnabled;
+        this.OSSEnabled = OSSEnabled;
+        this.internalAnalyzerEnabled = internalAnalyzerEnabled;
+    }
+
+    public AnalyzersConfig(){
+
+    }
+
+    public boolean isSnykEnabled() {
+        return snykEnabled;
+    }
+
+    public void setSnykEnabled(boolean snykEnabled) {
+        this.snykEnabled = snykEnabled;
+    }
+
+    public boolean isOSSEnabled() {
+        return OSSEnabled;
+    }
+
+    public void setOSSEnabled(boolean OSSEnabled) {
+        this.OSSEnabled = OSSEnabled;
+    }
+
+    public boolean isInternalAnalyzerEnabled() {
+        return internalAnalyzerEnabled;
+    }
+
+    public void setInternalAnalyzerEnabled(boolean internalAnalyzerEnabled) {
+        this.internalAnalyzerEnabled = internalAnalyzerEnabled;
+    }
+}
