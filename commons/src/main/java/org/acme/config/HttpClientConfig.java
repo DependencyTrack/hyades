55aae1614d27d2f50b2fcabe459f38906830ba9c
mehab <meha.bhargava2@gmail.com>
2023-01-12 15:28:32 +0000

    removed properties file from commons

--- commons/src/main/java/org/acme/config/HttpClientConfig.java
+++ commons/src/main/java/org/acme/config/HttpClientConfig.java
@@ -1,6 +1,7 @@
 package org.acme.config;
 
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 import java.util.Optional;
 
@@ -14,8 +15,13 @@ public interface HttpClientConfig {
 
     Optional<String> proxyPassword();
     Optional<String> noProxy();
+
+    @WithDefault("30")
     int proxyTimeoutConnection();
+    @WithDefault("60")
     int proxyTimeoutPool();
+
+    @WithDefault("30")
     int proxyTimeoutSocket();
 
 }
