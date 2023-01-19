13b5be18c9307142c15630e4b57c1ccc34d87037
Sahiba Mittal <sahibamittal98@gmail.com>
2023-01-19 14:59:27 +0000

    Update NvdMirrorHandler.java

--- mirror-service/src/main/java/org/acme/nvd/NvdMirrorHandler.java
+++ mirror-service/src/main/java/org/acme/nvd/NvdMirrorHandler.java
@@ -1,6 +1,5 @@
 package org.acme.nvd;
 
-import alpine.Config;
 import io.github.jeremylong.nvdlib.nvd.DefCveItem;
 import org.acme.client.NvdClient;
 import org.cyclonedx.model.Bom;
@@ -9,22 +8,16 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import static org.acme.util.FileUtil.setOutputDir;
 
 @ApplicationScoped
 public class NvdMirrorHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NvdMirrorHandler.class);
     private NvdClient client;
-    private File outputDir;
     List<Bom> nvdVulnerabilities;
-    public static final String NVD_MIRROR_DIR = Config.getInstance().getDataDirectorty().getAbsolutePath() + File.separator + "nist";
-
     @Inject
     public NvdMirrorHandler(final NvdClient client) {
         this.client = client;
@@ -34,8 +27,6 @@ public class NvdMirrorHandler {
     public List<Bom> performMirror() {
         final long start = System.currentTimeMillis();
         LOGGER.info("Starting NVD mirroring task");
-        final File mirrorPath = new File(NVD_MIRROR_DIR);
-        outputDir = setOutputDir(mirrorPath.getAbsolutePath());
         getNvdFeeds();
         final long end = System.currentTimeMillis();
         LOGGER.info("NVD mirroring complete");
