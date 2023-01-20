e4af913d3609c9760979616a2eff46953dd2183a
Sahiba Mittal <sahibamittal98@gmail.com>
2023-01-20 11:38:32 +0000

    Update MirrorServiceTopologyTest.java

--- mirror-service/src/test/java/org/acme/MirrorServiceTopologyTest.java
+++ mirror-service/src/test/java/org/acme/MirrorServiceTopologyTest.java
@@ -4,6 +4,7 @@ import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import org.acme.common.KafkaTopic;
+import org.acme.nvd.NvdMirrorHandler;
 import org.acme.osv.OsvMirrorHandler;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -32,16 +33,21 @@ public class MirrorServiceTopologyTest {
     @Inject
     Topology topology;
     private TopologyTestDriver testDriver;
-    private TestInputTopic<String, String> inputTopic;
+    private TestInputTopic<String, String> inputTopicOsv;
+    private TestInputTopic<String, String> inputTopicNvd;
     private TestOutputTopic<String, Bom> outputTopic;
     private OsvMirrorHandler osvMirrorHandlerMock;
+    private NvdMirrorHandler nvdMirrorHandlerMock;
 
     @BeforeEach
     void beforeEach() {
         osvMirrorHandlerMock = Mockito.mock(OsvMirrorHandler.class);
         QuarkusMock.installMockForType(osvMirrorHandlerMock, OsvMirrorHandler.class);
+        nvdMirrorHandlerMock = Mockito.mock(NvdMirrorHandler.class);
+        QuarkusMock.installMockForType(nvdMirrorHandlerMock, NvdMirrorHandler.class);
         testDriver = new TopologyTestDriver(topology);
-        inputTopic = testDriver.createInputTopic(KafkaTopic.MIRROR_OSV.getName(), new StringSerializer(), new StringSerializer());
+        inputTopicOsv = testDriver.createInputTopic(KafkaTopic.MIRROR_OSV.getName(), new StringSerializer(), new StringSerializer());
+        inputTopicNvd = testDriver.createInputTopic(KafkaTopic.MIRROR_NVD.getName(), new StringSerializer(), new StringSerializer());
         outputTopic = testDriver.createOutputTopic(KafkaTopic.NEW_VULNERABILITY.getName(), new StringDeserializer(), new ObjectMapperDeserializer<>(Bom.class));
     }
 
@@ -51,23 +57,33 @@ public class MirrorServiceTopologyTest {
     }
 
     @Test
-    void testNoAdvisories() throws IOException {
+    void testNoOsvAdvisories() throws IOException {
         Mockito.when(osvMirrorHandlerMock.performMirror(Mockito.anyString())).thenReturn(Collections.emptyList());
-        inputTopic.pipeInput("Maven", "");
+        inputTopicOsv.pipeInput("Maven", "");
         Assertions.assertTrue(outputTopic.isEmpty());
     }
 
     @Test
-    void testOsvMirroring() throws IOException {
-        Bom osvAdvisory = new Bom();
+    void testNoNvdAdvisories() {
+        Mockito.when(nvdMirrorHandlerMock.performMirror()).thenReturn(Collections.emptyList());
+        inputTopicNvd.pipeInput("", "");
+        Assertions.assertTrue(outputTopic.isEmpty());
+    }
+
+    @Test
+    void testMirroring() throws IOException {
+        Bom bov = new Bom();
         Vulnerability vulnerability = new Vulnerability();
         vulnerability.setId("test-id");
-        osvAdvisory.setVulnerabilities(List.of(vulnerability));
-        Mockito.when(osvMirrorHandlerMock.performMirror(Mockito.anyString())).thenReturn(List.of(osvAdvisory));
-        inputTopic.pipeInput("Go", "");
-        assertThat(outputTopic.getQueueSize()).isEqualTo(1);
-        assertThat(outputTopic.readRecord()).satisfies(record -> {
-            assertThat(record.key()).isEqualTo("OSV/test-id");
+        bov.setVulnerabilities(List.of(vulnerability));
+        Mockito.when(osvMirrorHandlerMock.performMirror(Mockito.anyString())).thenReturn(List.of(bov));
+        Mockito.when(nvdMirrorHandlerMock.performMirror()).thenReturn(List.of(bov));
+        inputTopicOsv.pipeInput("Go", "");
+        inputTopicNvd.pipeInput("", "");
+        assertThat(outputTopic.getQueueSize()).isEqualTo(2);
+        assertThat(outputTopic.readRecordsToList()).satisfies(records -> {
+            assertThat(records.get(0).getKey()).isEqualTo("OSV/test-id");
+            assertThat(records.get(1).getKey()).isEqualTo("NVD/test-id");
         });
     }
 }
