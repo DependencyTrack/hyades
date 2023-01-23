f4bba492a503c651b72b1ba29aabf2283de277b4
Sahiba Mittal <sahibamittal98@gmail.com>
2023-01-23 17:05:40 +0000

    Update FileUtil.java

--- mirror-service/src/main/java/org/acme/util/FileUtil.java
+++ mirror-service/src/main/java/org/acme/util/FileUtil.java
@@ -3,9 +3,6 @@ package org.acme.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -31,39 +28,5 @@ public class FileUtil {
         Path tempDir = Files.createTempDirectory(dirName);
         Path tempFile = Files.createTempFile(tempDir, "", fileSuffix);
         return tempFile;
-    }
-
-    /**
-     * Writes the modification time to a timestamp file
-     * @param file the file
-     * @param modificationTime the time of the last update
-     */
-    public static void writeTimeStampFile(File file, Long modificationTime)
-    {
-        FileWriter writer = null;
-        try {
-            writer= new FileWriter(file);
-            writer.write(Long.toString(modificationTime));
-        }
-        catch (IOException ex) {
-            LOGGER.error("An error occurred writing time stamp file", ex);
-        }
-        finally {
-            close(writer);
-        }
-    }
-
-    /**
-     * Closes a closable object.
-     * @param object the object to close
-     */
-    public static void close(Closeable object) {
-        if (object != null) {
-            try {
-                object.close();
-            } catch (IOException e) {
-                LOGGER.warn("Error closing stream", e);
-            }
-        }
     }
 }
