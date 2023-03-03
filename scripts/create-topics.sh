754acbac133f6fab8e4aa65bed1b68f4d5c5b46c
Niklas <nscuro@protonmail.com>
2023-03-03 13:39:31 +0100

    Provide empty fallback value for `API_TOPIC_PREFIX` in `create-topics.sh`
    Prevents the script from failing when `API_TOPIC_PREFIX` is not explicitly set
    
    Signed-off-by: Niklas <nscuro@protonmail.com>

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -16,42 +16,42 @@ rpk cluster health --watch --exit-when-healthy \
   --api-urls "$(echo "$REDPANDA_BROKERS" | sed -E 's/:[[:digit:]]+/:9644/g')"
 
 notification_topics=(
-  "${API_TOPIC_PREFIX}dtrack.notification.analyzer"
-  "${API_TOPIC_PREFIX}dtrack.notification.bom-consumed"
-  "${API_TOPIC_PREFIX}dtrack.notification.bom-processed"
-  "${API_TOPIC_PREFIX}dtrack.notification.configuration"
-  "${API_TOPIC_PREFIX}dtrack.notification.datasource-mirroring"
-  "${API_TOPIC_PREFIX}dtrack.notification.file-system"
-  "${API_TOPIC_PREFIX}dtrack.notification.indexing-service"
-  "${API_TOPIC_PREFIX}dtrack.notification.integration"
-  "${API_TOPIC_PREFIX}dtrack.notification.new-vulnerability"
-  "${API_TOPIC_PREFIX}dtrack.notification.new-vulnerable-dependency"
-  "${API_TOPIC_PREFIX}dtrack.notification.policy-violation"
-  "${API_TOPIC_PREFIX}dtrack.notification.project-audit-change"
-  "${API_TOPIC_PREFIX}dtrack.notification.repository"
-  "${API_TOPIC_PREFIX}dtrack.notification.vex-consumed"
-  "${API_TOPIC_PREFIX}dtrack.notification.vex-processed"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.analyzer"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.bom-consumed"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.bom-processed"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.configuration"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.datasource-mirroring"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.file-system"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.indexing-service"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.integration"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.new-vulnerability"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.new-vulnerable-dependency"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.policy-violation"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.project-audit-change"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.repository"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.vex-consumed"
+  "${API_TOPIC_PREFIX:-}dtrack.notification.vex-processed"
 )
 for topic_name in "${notification_topics[@]}"; do
   create_topic "$topic_name" "${NOTIFICATION_TOPICS_PARTITIONS:-3}" "retention.ms=${NOTIFICATION_TOPICS_RETENTION_MS:-43200000}"
 done
 
 repo_meta_analysis_topics=(
-  "${API_TOPIC_PREFIX}dtrack.repo-meta-analysis.component"
-  "${API_TOPIC_PREFIX}dtrack.repo-meta-analysis.result"
+  "${API_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.component"
+  "${API_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.result"
 )
 for topic_name in "${repo_meta_analysis_topics[@]}"; do
   create_topic "$topic_name" "${REPO_META_ANALYSIS_TOPICS_PARTITIONS:-3}" "retention.ms=${REPO_META_ANALYSIS_TOPICS_RETENTION_MS:-43200000}"
 done
 
 vuln_analysis_topics=(
-  "${API_TOPIC_PREFIX}dtrack.vuln-analysis.component"
-  "${API_TOPIC_PREFIX}dtrack.vuln-analysis.result"
+  "${API_TOPIC_PREFIX:-}dtrack.vuln-analysis.component"
+  "${API_TOPIC_PREFIX:-}dtrack.vuln-analysis.result"
 )
 for topic_name in "${vuln_analysis_topics[@]}"; do
   create_topic "$topic_name" "${VULN_ANALYSIS_TOPICS_PARTITIONS:-3}" "retention.ms=${VULN_ANALYSIS_TOPICS_RETENTION_MS:-43200000}"
 done
 
-create_topic "${API_TOPIC_PREFIX}dtrack.vulnerability.mirror.osv" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
-create_topic "${API_TOPIC_PREFIX}dtrack.vulnerability.mirror.nvd" "${VULN_MIRROR_TOPICS_PARTITIONS:-1}" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
-create_topic "${API_TOPIC_PREFIX}dtrack.vulnerability" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "cleanup.policy=compact"
+create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.osv" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
+create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.nvd" "${VULN_MIRROR_TOPICS_PARTITIONS:-1}" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
+create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "cleanup.policy=compact"
