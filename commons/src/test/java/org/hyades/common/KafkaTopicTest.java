c655d030067e37bc6816c50385e8bb89faa62640
Sahiba Mittal <sahibamittal98@gmail.com>
2023-03-03 16:25:28 +0000

    Update KafkaTopicTest.java

--- commons/src/test/java/org/hyades/common/KafkaTopicTest.java
+++ commons/src/test/java/org/hyades/common/KafkaTopicTest.java
@@ -1,16 +1,30 @@
 package org.hyades.common;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
+@TestProfile(KafkaTopicTest.TestProfile.class)
 public class KafkaTopicTest {
+
+    public static class TestProfile implements QuarkusTestProfile {
+
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of(
+                    "api.topic.prefix", "customPrefix."
+            );
+        }
+    }
 
     @Test
     void testKafkaTopicConfig() {
-        assertEquals("dtrack.vulnerability.mirror.osv", KafkaTopic.MIRROR_OSV.getName());
         System.setProperty("api.topic.prefix", "customPrefix.");
         assertEquals("customPrefix.dtrack.vulnerability.mirror.osv", KafkaTopic.MIRROR_OSV.getName());
     }
