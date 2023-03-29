77b7e375ac94685685b33699d87477387bd93e02
mehab <meha.bhargava2@gmail.com>
2023-03-29 16:26:42 +0100

    initial commit
    Signed-off-by: mehab <meha.bhargava2@gmail.com>

--- mirror-service-x/src/main/java/org/hyades/vulnmirror/datasource/AbstractDatasourceMirror.java
+++ mirror-service-x/src/main/java/org/hyades/vulnmirror/datasource/AbstractDatasourceMirror.java
@@ -85,7 +85,7 @@ public abstract class AbstractDatasourceMirror<T> implements DatasourceMirror {
         final byte[] serializedBov = bov.toByteArray();
         final byte[] bovDigest = DigestUtils.getSha256Digest().digest(serializedBov);
 
-        if (!Arrays.equals(vulnDigestStore.get(Datasource.NVD, vulnId), bovDigest)) {
+        if (!Arrays.equals(vulnDigestStore.get(datasource, vulnId), bovDigest)) {
             // TODO: Change back to debug; info is used for demonstration purposes only.
             logger.info("{} has changed", recordKey);
             bovProducer.send(new ProducerRecord<>(
