b69bae387904f3e2e8527531a57f7e233929d94b
mehab <meha.bhargava2@gmail.com>
2023-03-29 16:01:33 +0100

    added mirror service to the flow
    Signed-off-by: mehab <meha.bhargava2@gmail.com>

---
+++ mirror-service-x/src/main/java/org/hyades/vulnmirror/datasource/osv/OsvMirror.java
@@ -0,0 +1,95 @@
+package org.hyades.vulnmirror.datasource.osv;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.Producer;
+import org.cyclonedx.proto.v1_4.Bom;
+import org.hyades.vulnmirror.datasource.AbstractDatasourceMirror;
+import org.hyades.vulnmirror.datasource.Datasource;
+import org.hyades.vulnmirror.state.MirrorStateStore;
+import org.hyades.vulnmirror.state.VulnerabilityDigestStore;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.hyades.vulnmirror.datasource.util.FileUtil.deleteFileAndDir;
+
+@ApplicationScoped
+public class OsvMirror extends AbstractDatasourceMirror<Void> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OsvMirror.class);
+    private final ExecutorService executorService;
+    private final OsvClient client;
+    private final ObjectMapper objectMapper;
+
+    public OsvMirror(ExecutorService executorService, OsvClient client,
+                     @Named("osvObjectMapper") ObjectMapper objectMapper, final MirrorStateStore mirrorStateStore,
+                     final VulnerabilityDigestStore vulnDigestStore,
+                     final Producer<String, byte[]> bovProducer) {
+        super(Datasource.OSV, mirrorStateStore, vulnDigestStore, bovProducer, Void.class);
+        this.executorService = executorService;
+        this.client = client;
+        this.objectMapper = objectMapper;
+    }
+
+    public void performMirror(String ecosystem) {
+        try {
+            Path ecosystemZip = client.downloadEcosystemZip(ecosystem);
+            try (InputStream inputStream = new FileInputStream(ecosystemZip.toFile());
+                 ZipInputStream zipInput = new ZipInputStream(inputStream)) {
+                unzipFolder(zipInput);
+                deleteFileAndDir(ecosystemZip);
+                LOGGER.info("OSV mirroring completed for ecosystem: {}", ecosystem);
+            }
+        } catch (IOException e) {
+            LOGGER.error("Exception found while reading from OSV: ", e);
+        }
+    }
+
+    private void unzipFolder(ZipInputStream zipIn) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(zipIn));
+        try{
+        ZipEntry zipEntry = zipIn.getNextEntry();
+        while (zipEntry != null) {
+
+            String line = null;
+            StringBuilder out = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            var json = new JSONObject(out.toString());
+            Bom osvAdvisory = org.cyclonedx.proto.v1_4.Bom.newBuilder().build();//new OsvToCyclonedxParser(this.objectMapper).parse(json);
+            if (osvAdvisory != null) {
+                publishIfChanged(osvAdvisory);
+            }
+            zipEntry = zipIn.getNextEntry();
+            reader = new BufferedReader(new InputStreamReader(zipIn));
+        }
+        reader.close();
+    } catch (Exception ex) {
+            LOGGER.error("An error occurred while performing OSV mirroring", ex);
+        }
+    }
+
+    @Override
+    public void doMirror() {
+        executorService.submit(this::mirrorInternal);
+    }
+
+    private void mirrorInternal() {
+        performMirror(Datasource.OSV.name());
+    }
+
+}
---
+++ mirror-service-x/src/main/java/org/hyades/vulnmirror/datasource/util/FileUtil.java
@@ -0,0 +1,32 @@
+package org.hyades.vulnmirror.datasource.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUtil.class);
+    public static void deleteFileAndDir(Path filePath) {
+        try {
+            boolean fileDeleted = filePath.toFile().delete();
+            if (fileDeleted) {
+                boolean dirDeleted = filePath.getParent().toFile().delete();
+                if (!dirDeleted) {
+                    LOGGER.info("Error while deleting the directory path %s", filePath.getParent().getFileName().toString());
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.info("Error while deleting the file path %s : %s", filePath.getFileName().toString(), e);
+        }
+    }
+
+    public static Path getTempFileLocation(String dirName, String fileSuffix) throws IOException {
+        Path tempDir = Files.createTempDirectory(dirName);
+        Path tempFile = Files.createTempFile(tempDir, "", fileSuffix);
+        return tempFile;
+    }
+}
