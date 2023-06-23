ac415fdf9821e7a915991a69d851ee9526ee4058
nscuro <nscuro@protonmail.com>
2023-06-23 16:40:28 +0200

    Attempt to resolve `CodedOutputStream$OutOfSpaceException` in tests
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- proto/src/main/java/org/hyades/proto/KafkaProtobufSerializer.java
+++ proto/src/main/java/org/hyades/proto/KafkaProtobufSerializer.java
@@ -4,6 +4,9 @@ import com.google.protobuf.MessageLite;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class KafkaProtobufSerializer<T extends MessageLite> implements Serializer<T> {
 
     @Override
@@ -12,9 +15,10 @@ public class KafkaProtobufSerializer<T extends MessageLite> implements Serialize
             return null;
         }
 
-        try {
-            return data.toByteArray();
-        } catch (RuntimeException e) {
+        try (final var outStream = new ByteArrayOutputStream()) {
+            data.writeTo(outStream);
+            return outStream.toByteArray();
+        } catch (IOException | RuntimeException e) {
             throw new SerializationException(e);
         }
     }
