ca79376c5188b19a6fa7c0f3b34c3615844dcbaa
mehab <meha.bhargava@citi.com>
2023-07-12 15:14:14 +0100

    more logging added
    Signed-off-by: mehab <meha.bhargava@citi.com>

--- repository-meta-analyzer/src/test/java/org/hyades/repositories/MavenMetaAnalyzerTest.java
+++ repository-meta-analyzer/src/test/java/org/hyades/repositories/MavenMetaAnalyzerTest.java
@@ -23,10 +23,6 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.ResourceArg;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.TestProfile;
-import jakarta.ws.rs.core.MediaType;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpStatus;
 import org.apache.http.impl.client.HttpClients;
 import org.hyades.model.MetaModel;
 import org.hyades.persistence.model.Component;
@@ -36,26 +32,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
-import static com.github.tomakehurst.wiremock.client.WireMock.matching;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
-import static com.github.tomakehurst.wiremock.http.Body.fromJsonBytes;
-import static org.assertj.core.api.Assertions.assertThat;
-
 @QuarkusTest
 @QuarkusTestResource(
         value = WireMockTestResource.class,
         initArgs = @ResourceArg(name = "serverUrlProperty", value = "scanner.snyk.api.baseurl")
 )
-
 class MavenMetaAnalyzerTest {
 
     private IMetaAnalyzer analyzer;
@@ -112,21 +93,5 @@ class MavenMetaAnalyzerTest {
         MetaModel metaModel = analyzer.analyze(component);
         Assertions.assertEquals(RepositoryType.PYPI.name(), metaModel.getComponent().getPurl().getType().toUpperCase());
     }
-
-    @Test
-    void testNon200ResponseFromHttpClient() throws Exception {
-        analyzer.setRepositoryBaseUrl(wireMockServer.baseUrl());
-        wireMockServer.stubFor(post(urlPathMatching("/*"))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.SC_BAD_REQUEST)));
-        Component component = new Component();
-        component.setPurl(new PackageURL("pkg:maven/junit/junit@4.12"));
-
-        Assertions.assertEquals("MavenMetaAnalyzer", analyzer.getName());
-        Assertions.assertTrue(analyzer.isApplicable(component));
-        Assertions.assertEquals(RepositoryType.MAVEN, analyzer.supportedRepositoryType());
-        MetaModel metaModel = analyzer.analyze(component);
-
-
-    }
+}
 }
