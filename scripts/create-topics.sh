6e4d91ded8e86b5e92e3cea073f5e48f647560a9
Sahiba Mittal <sahiba.mittal@citi.com>
2024-02-21 10:31:56 +0000

    replaced remaining API_TOPIC_PREFIX

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -22,46 +22,46 @@ rpk cluster health --watch --exit-when-healthy \
   --api-urls "$(echo "$REDPANDA_BROKERS" | sed -E 's/:[[:digit:]]+/:9644/g')"
 
 notification_topics=(
-  "${API_TOPIC_PREFIX:-}dtrack.notification.analyzer"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.bom"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.configuration"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.datasource-mirroring"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.file-system"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.integration"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.new-vulnerability"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.new-vulnerable-dependency"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.policy-violation"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.project-audit-change"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.project-created"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.project-vuln-analysis-complete"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.repository"
-  "${API_TOPIC_PREFIX:-}dtrack.notification.vex"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.analyzer"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.bom"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.configuration"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.datasource-mirroring"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.file-system"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.integration"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.new-vulnerability"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.new-vulnerable-dependency"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.policy-violation"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.project-audit-change"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.project-created"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.project-vuln-analysis-complete"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.repository"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.notification.vex"
 )
 for topic_name in "${notification_topics[@]}"; do
   create_topic "$topic_name" "${NOTIFICATION_TOPICS_PARTITIONS:-1}" "retention.ms=${NOTIFICATION_TOPICS_RETENTION_MS:-43200000}"
 done
 
 repo_meta_analysis_topics=(
-  "${API_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.component"
-  "${API_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.result"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.component"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.repo-meta-analysis.result"
 )
 for topic_name in "${repo_meta_analysis_topics[@]}"; do
   create_topic "$topic_name" "${REPO_META_ANALYSIS_TOPICS_PARTITIONS:-3}" "retention.ms=${REPO_META_ANALYSIS_TOPICS_RETENTION_MS:-43200000}"
 done
 
 vuln_analysis_topics=(
-  "${API_TOPIC_PREFIX:-}dtrack.vuln-analysis.component"
-  "${API_TOPIC_PREFIX:-}dtrack.vuln-analysis.scanner.result"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.vuln-analysis.component"
+  "${KAFKA_TOPIC_PREFIX:-}dtrack.vuln-analysis.scanner.result"
 )
 for topic_name in "${vuln_analysis_topics[@]}"; do
   create_topic "$topic_name" "${VULN_ANALYSIS_TOPICS_PARTITIONS:-3}" "retention.ms=${VULN_ANALYSIS_TOPICS_RETENTION_MS:-43200000}"
 done
 
-create_topic "${API_TOPIC_PREFIX:-}dtrack.vuln-analysis.result" "${VULN_ANALYSIS_RESULT_TOPIC_PARTITIONS:-3}" "retention.ms=${VULN_ANALYSIS_RESULT_TOPIC_RETENTION_MS:-43200000}"
+create_topic "${KAFKA_TOPIC_PREFIX:-}dtrack.vuln-analysis.result" "${VULN_ANALYSIS_RESULT_TOPIC_PARTITIONS:-3}" "retention.ms=${VULN_ANALYSIS_RESULT_TOPIC_RETENTION_MS:-43200000}"
 
-create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.command" "1" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
-create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.state" "1" "cleanup.policy=compact segment.bytes=67108864 max.compaction.lag.ms=0"
-create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability.digest" "1" "cleanup.policy=compact segment.bytes=134217728"
-create_topic "${API_TOPIC_PREFIX:-}dtrack.vulnerability" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "cleanup.policy=compact"
+create_topic "${KAFKA_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.command" "1" "retention.ms=${VULN_MIRROR_TOPICS_RETENTION_MS:-43200000}"
+create_topic "${KAFKA_TOPIC_PREFIX:-}dtrack.vulnerability.mirror.state" "1" "cleanup.policy=compact segment.bytes=67108864 max.compaction.lag.ms=0"
+create_topic "${KAFKA_TOPIC_PREFIX:-}dtrack.vulnerability.digest" "1" "cleanup.policy=compact segment.bytes=134217728"
+create_topic "${KAFKA_TOPIC_PREFIX:-}dtrack.vulnerability" "${VULN_MIRROR_TOPICS_PARTITIONS:-3}" "cleanup.policy=compact"
 
 echo "All topics created successfully"
