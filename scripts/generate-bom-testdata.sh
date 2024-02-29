189f4cf87b24793367c5bc08b58368bfa0fcd6a9
nscuro <nscuro@protonmail.com>
2024-02-29 14:16:57 +0100

    Fix `generate-bom-testdata.sh` failing due to removed ZAP image
    ZAP is no longer an OWASP project, so their images have been removed from the `owasp` Docker Hub organization.
    
    Also update the script to use a Trivy container instead of a local installation. Avoids the overhead of having to install Trivy.
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- scripts/generate-bom-testdata.sh
+++ scripts/generate-bom-testdata.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
 SCRIPT_DIR="$(cd -P -- "$(dirname "$0")" && pwd -P)"
 BOM_TESTDATA_DIR="$(cd -P -- "${SCRIPT_DIR}/../testdata/boms" && pwd -P)"
 
-if ! command -v trivy &>/dev/null; then
-  echo "trivy was not found; ensure trivy is installed and re-run this script"
-  exit 2
-fi
-
 while read -r image_name; do
-    trivy image -f cyclonedx -o "${BOM_TESTDATA_DIR}/generated/container_$(echo "${image_name}" | sed 's/\//_/g' | sed 's/:/_/g').cdx.json" "${image_name}"
+    filename="container_$(echo "${image_name}" | sed 's/\//_/g' | sed 's/:/_/g').cdx.json"
+    docker run -i --rm --user "$(id -u):$(id -g)" \
+      -v "${HOME}/.cache/trivy:/tmp/trivy-cache" \
+      -v "${BOM_TESTDATA_DIR}:/tmp/trivy-out" aquasec/trivy:0.49.1 \
+      image --cache-dir /tmp/trivy-cache -f cyclonedx \
+      -o "/tmp/trivy-out/generated/${filename}" \
+      "${image_name}" </dev/null
 done < "${BOM_TESTDATA_DIR}/generated/images.txt"
