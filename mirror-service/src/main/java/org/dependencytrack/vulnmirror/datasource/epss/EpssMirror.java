1a450df11f86ee9885f8c6663e40dd39ad68368e
Sahiba Mittal <sahiba.mittal@citi.com>
2024-03-15 17:05:36 +0000

    Update EpssMirror.java

--- mirror-service/src/main/java/org/dependencytrack/vulnmirror/datasource/epss/EpssMirror.java
+++ mirror-service/src/main/java/org/dependencytrack/vulnmirror/datasource/epss/EpssMirror.java
@@ -64,7 +64,7 @@ class EpssMirror extends AbstractDatasourceMirror<Void> {
         });
     }
 
-    void performMirror() throws Exception {
+    void performMirror() throws Throwable {
         final Timer.Sample durationSample = Timer.start();
         try {
             final EpssDataFeed epssClient = epssClientFactory.create();
