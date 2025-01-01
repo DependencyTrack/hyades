d97989dc8a36708051d52b162754f3e3e4a14bde
Thorben Schmitt <thorben.schmitt@stud.h-da.de>
2025-01-01 18:27:05 +0100

    add locale functionality and global setup
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

---
+++ e2e/playwright-tests/setup/preconditions.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, type Page } from '@playwright/test';
+
+// auch locale selection
+// hier denke ich auch storage state mit neuen usern etc
+
+// --> das aber denke ich ebenfalls mit BDD
+
+// PRECONDITIONS SIND LOGIN
+test('Should log password', async ({ page }) => {
+    const pw = process.env.RANDOM_PASSWORD;
+    // const json = JSON.parse(process.env.LOCALE_JSON);
+
+    console.log(pw);
+    // console.log(json);
+});
