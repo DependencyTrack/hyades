bf83c33f52dec8cbefeef9b597956bace15464bb
Thorben Schmitt <thorben.schmitt@stud.h-da.de>
2025-01-26 22:21:35 +0100

    add precondition steps v0.9

---
+++ e2e/playwright-tests/page-objects/navigation-bar.pom.ts
@@ -0,0 +1,147 @@
+import { Page, Locator, expect } from '@playwright/test';
+import { getValue } from "../utilities/utils";
+
+export class NavigationParPage {
+    page: Page;
+    dashboardTab: Locator;
+    projectsTab: Locator;
+    componentsTab: Locator;
+    vulnerabilitiesTab: Locator;
+    licencesTab: Locator;
+    tagsTab: Locator;
+    vulnerabilityAuditTab: Locator;
+    policyManagementTab: Locator;
+    administrationTab: Locator;
+    navBarToggle: Locator;
+    sideBarMinimizer: Locator;
+
+    accountDropDown: Locator;
+    accountDropDownUpdatePassword: Locator;
+    accountDropDownChangePassword: Locator;
+    accountDropDownChangeLanguage: Locator;
+    accountDropDownLogout: Locator;
+
+    updateProfilePopup: Locator;
+    updateProfilePopupUsernameInput: Locator;
+    updateProfilePopupEmailInput: Locator;
+    updateProfilePopupCloseButton: Locator;
+    updateProfilePopupUpdateButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.dashboardTab = page.locator('/dashboard');
+        this.projectsTab = page.locator('/projects');
+        this.componentsTab = page.locator('/components');
+        this.vulnerabilitiesTab = page.locator('/vulnerabilities');
+        this.licencesTab = page.locator('/licences');
+        this.tagsTab = page.locator('/tags');
+        this.vulnerabilityAuditTab = page.locator('/vulnerabilityAudit');
+        this.policyManagementTab = page.locator('/policy');
+        this.administrationTab = page.locator('/admin');
+        this.navBarToggle = page.locator('button.d-md-down-none.navbar-toggler');
+        this.sideBarMinimizer = page.locator('button.sidebar-minimizer');
+
+        // Account Dropdown top right
+        this.accountDropDown = page.locator('li.dropdown'); //todo if not working a.dropdown-toggle aber dann alle darunter anpassen
+        this.accountDropDownUpdatePassword = this.accountDropDown.getByText(getValue("message", "logout"));
+        this.accountDropDownChangePassword = this.accountDropDown.locator('/change-password');
+        this.accountDropDownChangeLanguage = this.accountDropDown.locator('#locale-picker-form').locator('.custom-select');
+        this.accountDropDownLogout = this.accountDropDown.getByText(getValue("message", "profile_update"));
+
+        // Update Profile Popup
+        this.updateProfilePopup = page.locator('.modal-content');
+        this.updateProfilePopupUsernameInput = this.updateProfilePopup.locator('#fullname-input-input');
+        this.updateProfilePopupEmailInput = this.updateProfilePopup.locator('#email-input-input');
+        this.updateProfilePopupCloseButton = this.updateProfilePopup.getByText(getValue("message", "close"));
+        this.updateProfilePopupUpdateButton = this.updateProfilePopup.getByText(getValue("message", "update"));
+    }
+
+    async logout() {
+        await this.clickOnAccountDropDown();
+        await this.clickOnaccountDropDownLogout();
+    }
+
+    async clickOnDashboardTab() {
+        await this.dashboardTab.click();
+    }
+
+    async clickOnProjectsTab() {
+        await this.projectsTab.click();
+    }
+
+    async clickOnComponentsTab() {
+        await this.componentsTab.click();
+    }
+
+    async clickOnVulnerabilitiesTab() {
+        await this.vulnerabilitiesTab.click();
+    }
+
+    async clickOnLicencesTab() {
+        await this.licencesTab.click();
+    }
+
+    async clickOnTagsTab() {
+        await this.tagsTab.click();
+    }
+
+    async clickOnVulnerabilityAuditTab() {
+        await this.vulnerabilityAuditTab.click();
+    }
+
+    async clickOnPolicyManagementTab() {
+        await this.policyManagementTab.click();
+    }
+
+    async clickOnAdministrationTab() {
+        await this.administrationTab.click();
+    }
+
+    async clickOnNavBarToggler() {
+        await this.navBarToggle.click();
+    }
+
+    async clickOnSideBarMinimizer() {
+        await this.sideBarMinimizer.click();
+    }
+
+    async clickOnAccountDropDown() {
+        await this.accountDropDown.click();
+    }
+
+    async clickOnaccountDropDownUpdatePassword() {
+        await this.accountDropDownUpdatePassword.click();
+    }
+
+    async clickOnaccountDropDownChangePassword() {
+        await this.accountDropDownChangePassword.click();
+    }
+
+    async clickOnaccountDropDownChangeLanguage() {
+        await this.accountDropDownChangeLanguage.click();
+    }
+
+    async clickOnaccountDropDownLogout() {
+        await this.accountDropDownLogout.click();
+    }
+
+    async fillupdateProfilePopupUsernameInput(username: string) {
+        await this.updateProfilePopupUsernameInput.fill(username);
+    }
+
+    async fillupdateProfilePopupEmailInput(email: string) {
+        await this.updateProfilePopupEmailInput.fill(email);
+    }
+
+    async clickupdateProfilePopupCloseButton() {
+        await this.updateProfilePopupCloseButton.click();
+    }
+
+    async clickupdateProfilePopupUpdateButton() {
+        await this.updateProfilePopupUpdateButton.click();
+    }
+
+    async expectUpdateProfilePopupToBeVisible() {
+        await expect(this.updateProfilePopup).toBeVisible();
+    }
+}
--- e2e/playwright-tests/setup/preconditions.ts
+++ e2e/playwright-tests/setup/preconditions.ts
@@ -1,5 +1,8 @@
 import { test as setup } from '@playwright/test';
-import {LoginPage, PasswordChangePage} from "../page-objects/login.pom";
+import { LoginPage, PasswordChangePage } from "../page-objects/login.pom";
+import {AccessManagementMenu, AdministrationPage} from "../page-objects/administration.pom";
+import {NavigationParPage} from "../page-objects/navigation-bar.pom";
+import {NotificationPage} from "../page-objects/notification.pom";
 
 setup('Initial Login', async ({ page }) => {
     const loginPage = new LoginPage(page);
@@ -13,10 +16,22 @@ setup('Initial Login', async ({ page }) => {
     await page.waitForTimeout(2000);
 });
 
+
+const users = [
+    { username: 'test-user1', userFile: 'playwright-tests/.auth/user1.json', description: 'authenticate as user1', permissions: ["xyz", "xyz"], },
+    { username: 'test-user2', userFile: 'playwright-tests/.auth/user2.json', description: 'authenticate as user2', permissions: ["xyz", "xyz"], },
+    { username: 'test-user3', userFile: 'playwright-tests/.auth/user3.json', description: 'authenticate as user3', permissions: ["xyz", "xyz"], },
+    { username: 'test-user4', userFile: 'playwright-tests/.auth/user4.json', description: 'authenticate as user4', permissions: ["xyz", "xyz"], },
+    { username: 'test-user5', userFile: 'playwright-tests/.auth/user5.json', description: 'authenticate as user5', permissions: ["xyz", "xyz"], },
+];
 const adminFile = 'e2e/playwright-tests/.auth/admin.json';
 setup('authenticate as admin', async ({ page }) => {
     const loginPage = new LoginPage(page);
     // const dashboardPage = new DashboardPage(page);
+    const navBarPage = new NavigationParPage(page);
+    const administrationPage = new AdministrationPage(page);
+    const accessManagementMenu = new AccessManagementMenu(page);
+    const notificationPage = new NotificationPage(page);
 
     await page.goto('/');
 
@@ -27,19 +42,25 @@ setup('authenticate as admin', async ({ page }) => {
 
     // await dashboardPage.isDashboardPageVisible();
     // await administrationPage
-    // create users with groups
+    // todo create users nachdem mit Niklas gesprochen
+
+    await navBarPage.clickOnAdministrationTab();
+    await administrationPage.clickOnAdminMenu('AccessManagement');
+
+    for(const username of users) {
+        await accessManagementMenu.createUser(username.username, process.env.RANDOM_PASSWORD);
+        // todo await notificationPage.verifyUserCreatedAlert();
+        await page.waitForTimeout(1000);
+        // todo await accessManagementMenu.addPermissionsToUser(username.username, username.permissions);
+        await page.waitForTimeout(1000);
+    }
+
+    await navBarPage.clickOnDashboardTab();
 
     await page.context().storageState({ path: adminFile });
 });
 
-/*
-const users = [
-    { username: 'test-user1', userFile: 'playwright-tests/.auth/user1.json', description: 'authenticate as user1' },
-    { username: 'test-user2', userFile: 'playwright-tests/.auth/user2.json', description: 'authenticate as user2' },
-    { username: 'test-user3', userFile: 'playwright-tests/.auth/user3.json', description: 'authenticate as user3' },
-    { username: 'test-user4', userFile: 'playwright-tests/.auth/user4.json', description: 'authenticate as user4' },
-    { username: 'test-user5', userFile: 'playwright-tests/.auth/user5.json', description: 'authenticate as user5' },
-];
+
 
 const authenticateUser = async (username: string, userFile: string, description: string) => {
     setup(description, async ({ page }) => {
@@ -59,6 +80,5 @@ const authenticateUser = async (username: string, userFile: string, description:
 };
 
 users.forEach(({ username, userFile, description }) => {
-    authenticateUser(username, userFile, description);
+    authenticateUser(username, userFile, description).then(() => {});
 });
-*/
