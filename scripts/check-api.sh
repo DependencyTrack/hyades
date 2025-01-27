d3b1cc517440faeb4c29c39faadc084fd10b2608
Thorben Schmitt <thorben.schmitt@stud.h-da.de>
2025-01-27 14:42:47 +0100

    add precondition steps v0.96
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

--- scripts/check-api.sh
+++ scripts/check-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Skript zum Überprüfen, ob der API-Server mit HTTP 200 antwortet
-for i in {1..30}; do
+for i in {1..6}; do
   HTTP_CODE=$(curl --insecure -sL -w '%{http_code}' http://localhost:8081/health -o /dev/null)
   if [ "$HTTP_CODE" == "200" ]; then
     echo "API server is responding with HTTP 200!"
