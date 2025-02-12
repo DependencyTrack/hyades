42c8c0e24ab5266e0121efef3feea7689faf2c62
Thorben Schmitt <thorben.schmitt@stud.h-da.de>
2025-02-12 23:22:16 +0100

    add permission feature
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

--- e2e/playwright-tests/setup/auth-setup.ts
+++ e2e/playwright-tests/setup/auth-setup.ts
@@ -12,8 +12,6 @@ setup('Store Admin Authentication', async ({ page }) => {
 
     await page.waitForURL('http://localhost:8081/dashboard', { timeout: 5000 });
     await page.context().storageState({ path: adminFile });
-    // todo currently not working..fix with sessionStorage https://playwright.dev/docs/auth#session-storage
-    //  add in common.steps when fixed and change playwright.config
     const sessionStorage = await page.evaluate(() => JSON.stringify(sessionStorage));
     fs.writeFileSync(adminFile, sessionStorage, 'utf-8');
 });
