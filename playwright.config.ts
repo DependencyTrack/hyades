140a3d4f99b5e5de3c485ccdc7dcdc65b9b75b59
Thorben Schmitt <thorben.schmitt@stud.h-da.de>
2025-02-15 17:59:29 +0100

    add permission feature
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["dots"],
+  reporter: process.env.CI ? [["dot"],
     ["allure-playwright",
       {
         resultsDir: defOutDir + "/allure-results",
