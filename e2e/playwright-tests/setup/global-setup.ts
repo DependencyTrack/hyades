e0275762152ecf4ea01534bf3fd51debac6a1b14
elAlmani <169059276+elAlmani@users.noreply.github.com>
2025-02-17 12:07:22 +0100

    Feature/add features for projects (#26)
    * fix linting
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

--- e2e/playwright-tests/setup/global-setup.ts
+++ e2e/playwright-tests/setup/global-setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { request } from '@playwright/test';
 import * as fs from 'fs';
 import * as path from "node:path";
--- e2e/playwright-tests/steps/administration.steps.ts
+++ e2e/playwright-tests/steps/administration.steps.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { Then } from '../fixtures/fixtures';
 import { DataTable } from 'playwright-bdd';
 import {expect} from "@playwright/test";
